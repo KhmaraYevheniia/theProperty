ff6cbaf94ddbef87e174f744727b4b3ddf160732
nastya218 <anastasia.syrovatskaa@gmail.com>
2021-06-04 23:02:14 +0300

    create PropertyObject model

--- rega/models.py
+++ rega/models.py
@@ -24,3 +24,12 @@ class User(AbstractUser):
 
     def full_name(self):
         return self.first_name +' '+self.last_name
+
+class PropertyObject(models.Model):
+    latitude = models.FloatField('latitude')
+    longitude = models.FloatField('longitude')
+    square = models.FloatField('square')
+    price = models.FloatField('price')
+    type = models.CharField('type', max_length=100)
+    address = models.CharField('address', max_length=300)
+    sold_status = models.BooleanField('sold_status', default=False)
